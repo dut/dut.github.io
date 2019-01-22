f749fc2836448d97bd17bdbbea69082201bea181
Nicola <nicoladutartre@gmail.com>
2019-01-22 10:43:46 +0100

    Options fun du bouton inutile

--- script.js
+++ script.js
@@ -38,15 +38,4 @@ if (clicks == 5){
   document.getElementById("uselessbutton").style.display = "None";
   document.write("Bravo tu as cliqué 100 fois. J'espère que tu es content parce que le bouton fait la gueule maintenant.");
 }
-//else if (clicks == 20) {
-//  document.getElementById("uselessbutton").innerHTML ="Vraiment. Arrête.");
-//}
-
-//else if (clicks >= 30) {
-//  document.getElementById("message").innerHTML = "STOP.";
-//}
-//else if(clicks >= 100) {
-//  document.write("Bravo tu as cliqué 100 fois. J'espère que tu es content.");
-//
-//}
 };
